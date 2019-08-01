9a5f4fc14e305a52dfaf96d1ef3c9a71b420564c
Larry Joel Garcia Paredes <lgarcia@lan.wundermanphantasia.pe>
2019-08-01 10:52:19 -0500

    Estructura base para Portafolio

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <BaseTemplate/>
+  </div>
+</template>
+
+<script>
+import BaseTemplate from './components/BaseTemplate'
+
+export default {
+  name: 'App',
+  components: {
+    BaseTemplate
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
